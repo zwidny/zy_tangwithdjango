90ccc35766d3fe4722ab830713c83530d2cf572b
zwidny <649038269@qq.com>
2015-05-10 22:18:21 +0800

    ReInitial Project

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'boldmessage': "Bold Messages",
+    }
+    return render(request,
+                  'rango/index.html',
+                  context
+                  )
+
+
+def about(request):
+    return HttpResponse("About")
